a2ef2aa7de7de02fbf3c62ae47a669ea8d2115a3
CarlosSousa2001 <carlosramos10k@gmail.com>
2024-03-04 15:03:06 -0300

    criando icon e mudando fonte global

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"], variable:'--font-inter' });
+const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
 
 export const metadata: Metadata = {
   title: "DevStore",
@@ -14,8 +14,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.variable}>{children}</body>
+    <html lang="pt" className={inter.variable}>
+      <body>{children}</body>
     </html>
   );
 }
